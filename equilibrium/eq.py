9e4f3573a39324364a17669db87e29c07f40cb2b
Nandan Rao <nandanmarkrao@gmail.com>
2018-06-11 22:15:15 +0200

    BeliefTree Implementation!

--- equilibrium/eq.py
+++ equilibrium/eq.py
@@ -1,4 +1,13 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sympy import Symbol, Eq, solveset
+
+class InequilibriumError(BaseException):
+    pass
+
+class AssumptionError(BaseException):
+    pass
 
 def match_fn(unemployed, vacancies, aH):
     B = aH/3
@@ -12,8 +21,46 @@ def flatten(l):
             (flatten(l[1:]) if len(l) > 1 else [])
             if type(l) is list else [l])
 
-class Foo:
-    def __init__(self, aH, aL, y, c, b, delta, sigma, r, match_fn, precision = .1):
+class BeliefTree:
+    def __init__(self, m, unemployed, employed = [0.,0.]):
+        self.m = m
+        self.unemployed = np.array(unemployed)
+        self.employed = np.array(employed)
+        self.x = None
+        self.children = []
+
+    def add_market(self, x):
+        self.x = x
+
+    def add_children(self, low, high):
+        self.children = [low, high]
+
+    def __str__(self, level=1):
+        uL, uH = self.unemployed
+        eL, eH = self.employed
+        ret = "   " * (level-1) + "|--" + \
+              'm: {0:.2f}, u: {1:.2f}, e: {2:.2f} \n'.format(self.m, uL, eL)
+        for child in self.children:
+            ret += child.__str__(level+1)
+        return ret
+
+    def get_amounts(self, v):
+        e = getattr(self, v)
+        for c in self.children:
+            e += c.get_amounts(v)
+        return e
+
+    def __repr__(self):
+        return str(self)
+
+    def __len__(self):
+        if self.children:
+            return 1 + len(self.children[0])
+        return 1
+
+
+class Equilibrium:
+    def __init__(self, aH, aL, y, c, b, delta, sigma, r, B, precision = .1, distortion = 0.0):
         self.aH = aH
         self.aL = aL
         self.c = c
@@ -23,20 +70,82 @@ class Foo:
         self.sigma = sigma
         self.r = r
         self.b = b
+        self.B = B
+        self.distortion = distortion
 
         self.A = (self.r + self.sigma)/(1 - self.sigma) + self.delta
-        self.B = aH/3
-        # Note: we are not including 0!!
+
+
+        # Note: we are not including 0!
         self.X = np.around(np.arange(0, 1/aH, precision), 4)[1:]
         self._values = np.zeros(len(self.X))
 
         self.pH = .5
         self.pL = 1 - self.pH
-        self._unemployed = { '0.5': [self.pL * self.sigma, self.pH * self.sigma ]}
+        self.m0 = self.aH * self.pH + self.aL * self.pL
+        self._unemployed = { self.m0 : [self.pL * self.sigma, self.pH * self.sigma ]}
+        self._employed = { self.m0 : [0,0 ]}
+        self._wages = {}
+
+        self.belief_tree = BeliefTree(self.m0, [self.pL*self.sigma, self.pH*self.sigma])
+
+        # Check Assumption #2 regarding labor productivity, which ensures
+        # workers will always accept matches
+        self.assumption_2()
+
+        # Check that delta is small enough for Assumption #3
+        self.assumption_3()
+
+        if self.aH <= self.aL:
+            raise AssumptionError('High skill must be higher than low skill!')
+
+        if self.B >= aH or self.B <= 0:
+            raise AssumptionError('B must be between 0 and aH!. B: {}. aH: {}'.format(self.B, self.aH))
+
         # self._employed = { str(self.pos_win(0.5)) : self.currently_employed}
         # self._vacancies = np.zeroes(len(self.X))
         # self._unemployed = np.zeroes(len(self.X))
 
+    def tightness(self, x):
+        # try:
+        #     return self.vacancies(x)/self.unemployed(x)
+        # except ZeroDivisionError:
+        #     print("WHY DIS BE ZERO??")
+            # return 10 # RETURN SOMETHING ELSE?
+        return x/(1 - self.B*x)
+
+    def tightness_prime(self, y):
+        x = Symbol('x')
+        return self.tightness(x).diff().subs(x, y)
+
+    def x_star(self):
+        x = Symbol('x')
+        e = Eq(self.tightness(x).diff() - self.aH * self.tightness(1/self.aH))
+        l = list(solveset(e))
+        return [i for i in l if (i > 0) and (i < 1/self.aH)][0]
+
+    def Omega(self):
+        d = Symbol('d')
+        r,s = self.r, self.sigma
+        aL,aH = self.aL, self.aH
+        c = (r + s)/(1 - s)
+        b = (1 + aL/aH) * (c + d) + aL/aH
+        e = Eq( c * (c + d)**2 - d*b )
+        l = [x for x in list(solveset(e)) if x > 0]
+        return l[0]
+
+    def assumption_2(self):
+        left = (self.y - self.b)/self.c
+        x_star = self.x_star()
+        right = ((self.A + self.aH*x_star)*self.tightness_prime(x_star)
+                 - self.aH*self.tightness(x_star))
+        if left <= right:
+            raise AssumptionError('Assumption 2 not satisfied. {} is not greater than {}'.format(left, right))
+
+    def assumption_3(self):
+        d_bar = self.Omega()
+        if self.delta > d_bar:
+            raise AssumptionError('Assumption 3 is not satisfied. {} is greater than d_bar: {}'.format(self.delta, d_bar))
 
     def get_x(self, x):
         """Gets the index in self.X of a given market x"""
@@ -53,14 +162,6 @@ class Foo:
 
 
 
-    def tightness(self, x):
-        # try:
-        #     return self.vacancies(x)/self.unemployed(x)
-        # except ZeroDivisionError:
-        #     print("WHY DIS BE ZERO??")
-            # return 10 # RETURN SOMETHING ELSE?
-        return x/(1 - self.B*x)
-
     def wage(self, x):
         # One annoying part of this model is how wages go down
         # as vacancies go up... to "induce" firms into a tighter market.
@@ -70,15 +171,20 @@ class Foo:
         try:
             return self.y - self.c*self.A*self.tightness(x)/x
         except ZeroDivisionError:
-            return
+            pass
 
     def pos_win(self, m):
         aH, aL = self.aH, self.aL
-        return aH + aL - aH*aL/m
+        bayesian = aH + aL - aH*aL/m
+        return np.round(bayesian, 6)
 
     def pos_lose(self, x, m):
         aH, aL = self.aH, self.aL
-        return aH - (aH - m)*(1 - x*aL)/(1 - x*m)
+        bayesian = aH - (aH - m)*(1 - x*aL)/(1 - x*m)
+        behavioral = bayesian - (m - aL)*self.distortion
+        # update = bayesian - m
+        # behavioral = max(aL + eps, m + update * (1 + self.distortion))
+        return np.round(behavioral, 6)
 
     def pick_market(self):
         i = np.argmax(self._values)
@@ -105,64 +211,109 @@ class Foo:
             prev = np.copy(self._values)
             for x in self.X:
                 self._values[self.get_x(x)] = self.value_of_market(x, m)
-            e = np.max(prev - self._values)
+            e = np.max(np.abs((prev - self._values)))
         return self._values
 
-    def equilibrium_tree(self, m, it = 0, max_depth = 4):
-        self.it(m) # this feels really mutable and ugly
-        x = self.pick_market()
-        mL, mH = self.pos_lose(x,m), self.pos_win(m)
-        if it >= max_depth:
-            return [mL, mH]
-        return [m, self.equilibrium_tree(mL, it+1), self.equilibrium_tree(mH, it+1)]
-
     def unemployed(self, m):
-        return self._unemployed[str(m)]
+        return self._unemployed[m]
 
-    def employed_from(self, m):
-        self.it(m)
-        pos = self.pos_win(m)
-        x = self.pick_market()
-        uL, uH = self.unemployed(m)
+    def update_stats(self, d, key, val):
+        try:
+            d[key] += np.array(val)
+        except KeyError:
+            d[key] = np.array(val)
+
+    def employed_with_belief(self, x, mn, uL, uH):
         eq = lambda a,u: ((1 - self.sigma) * a * x * u) / (self.sigma + (1 - self.sigma)*self.delta)
-        L,H = [eq(a,u) for a,u in [(self.aL, uL), (self.aH, uH)]]
-        return (pos, [L,H])
+        return [eq(a,u) for a,u in [(self.aL, uL), (self.aH, uH)]]
 
-    def unemployed_rejects(self, m):
-        # for any m
-        self.it(m)
-        x = self.pick_market()
-        mn = self.pos_lose(x, m)
-        # traverse tree and generate all unemployed...
-        uL, uH = self.unemployed(m)
-        L,H = [(1 - self.sigma)*(1 - a * x)*u for a,u in [(self.aL, uL), (self.aH, uH)]]
-        return (mn, [L,H])
+    def unemployed_rejects(self, x, mn, uL, uH):
+        eq = lambda a,u: (1 - self.sigma)*(1 - a * x)*u
+        return [eq(a,u) for a,u in [(self.aL, uL), (self.aH, uH)]]
 
-    def fired_employees(self, m):
-        # for any m
-        self.it(m)
+    def fired_employees(self, employed):
         eq = lambda e: (1 - self.sigma) * self.delta * e
-        return [eq(e) for e in self.employed_from(m)[1]]
+        return [eq(e) for e in employed]
 
-    def build_distributions(self, tree):
-        m0 = tree[0]
-        m1, amts = self.unemployed(m0)
-        self._unemployed[m1] = amts
+    def equilibrium_tree(self, max_depth = 4, tree = None, it = 1):
+        if not tree:
+            tree = self.belief_tree
 
-        rejects = get_type(tree, rejects=True)
-        for m in rejects:
-            self._unemployed[str(m)] = self.unemployed_rejects(m)[1]
+        # Base case
+        if it >= max_depth:
+            self._equilibrium = True
+            return tree
+
+        # Unemployed with current beliefs
+        # your double updating!!! Get from tree...
+        curr_uL, curr_uH = tree.unemployed
+
+        # this feels really mutable and ugly
+        m = tree.m
+        self.it(m)
+
+        # Given these beliefs, x is the optimum market
+        x = self.pick_market()
+
+        # Next beliefs in the tree
+        mL = self.pos_lose(x, m)
+        mH = self.pos_win(m)
+
+        # Determine employed and unemployed amounts
+        uL = self.unemployed_rejects(x, mL, curr_uL, curr_uH)
+        eH = self.employed_with_belief(x, mH, curr_uL, curr_uH)
+        uH = self.fired_employees(eH)
+
+        # # update state
+        # if mL != m:
+        #     self.update_stats(self._unemployed, mL, uL)
+        if mH != m:
+            self.update_stats(self._wages, x, eH)
+        #     self.update_stats(self._unemployed, mH, uH)
+        #     self.update_stats(self._employed, mH, eH)
+        low = BeliefTree(mL, uL)
+        high = BeliefTree(mH, uH, eH)
+
+        # Recurse
+        tree.add_children(self.equilibrium_tree(max_depth, low, it+1),
+                          self.equilibrium_tree(max_depth, high, it+1))
+        return tree
+
+    def equilibrium(self, max_depth = 4):
+        self._belief_tree = self.equilibrium_tree(None, 0, max_depth)
+
+    def _flatten(self, z):
+        return [ x for y in z for x in y ]
+
+    def get_unemployment(self):
+        if not self._belief_tree:
+            raise InequilibriumError('You must first find equilibrium before getting unemployment')
+
+        u = sum(self._flatten(self._unemployed.values()))
+        e = sum(self._flatten(self._employed.values()))
+        return u/(u+e)
+
+    def plot_wages(self, skill = 'H'):
+        if not self._belief_tree:
+            raise InequilibriumError('You must first find equilibrium before getting unemployment')
+
+        i = 0 if skill == 'L' else 1
+        amts = [w[i] for w in self._wages.values()]
+        arr = sorted(list(zip(self._wages.keys(), amts)),
+                     key = lambda x: -x[0])
+        return pd.DataFrame(arr).plot.bar(x = 0, y = 1)
 
 
-def get_type(tree, rejects=True):
-    try:
-        L,H = tree[1], tree[2]
-        target = L if rejects else H
-        if len(target) > 2:
-            return [target[0]] + get_type(L, rejects) + get_type(H, rejects)
-        else: # target is a leaf
-            return get_type(L, rejects) + get_type(H, rejects)
-    except IndexError: # tree is leaf
-        return [tree[0]] if rejects else [tree[1]]
+## INTERACTIVE
+# def show_one(self):
+#     fig = self.get_figure()
+#     fig.savefig("/tmp/chart.png", dpi=90)
+#     fig.clear()
 
-# employed workers with type pos_win(m)
+# import matplotlib
+# matplotlib.artist.Artist.show_one = show_one
+
+# def test_values(**kwargs):
+#     foo = Equilibrium(**kwargs)
+#     foo.equilibrium()
+#     return foo.get_unemployment()
--- equilibrium/test_eq.py
+++ equilibrium/test_eq.py
@@ -1,75 +1,64 @@
 from eq import *
-
-# def test_tightness_returns_max():
-#     foo = Foo(.9, .1, 15, .1, .05, .1, .2, .04, match_fn, precision = .05)
-#     assert(foo.tightness(.05) == 10) # max tightness 10??
+import pytest
+from numpy.testing import assert_approx_equal
 
 def test_wage_returns_lower_value_with_higher_x():
-    foo = Foo(.9, .1, 15, .1, .05, .1, .2, .04, match_fn, precision = .05)
+    foo = Equilibrium(1, .05, .1, .05, .05, delta = .1, sigma = .2, r = .04, B=.3, precision = .05)
     foo.it(1.5)
     assert(foo.wage(.05) > foo.wage(.15))
 
 def test_pos_win_raises_m():
-    foo = Foo(.9, .1, 15, .1, .05, .1, .2, .04, match_fn, precision = .05)
+    foo = Equilibrium(1, .05, .1, .05, .05, delta = .1, sigma = .2, r = .04, B=.3, precision = .05)
     assert(foo.pos_win(.5) > .5)
-    assert(foo.pos_win(.5) < .9)
+    assert(foo.pos_win(.5) < 1.)
 
 def test_pos_lose_lowers_m():
-    foo = Foo(.9, .1, 15, .1, .05, .1, .2, .04, match_fn, precision = .05)
+    foo = Equilibrium(1, .05, .1, .05, .05, delta = .1, sigma = .2, r = .04, B=.3, precision = .05)
     assert(foo.pos_lose(.05, .5) < .5)
     assert(foo.pos_lose(.05, .5) > .1)
 
 def test_pick_market_different_for_beliefs():
-    foo = Foo(.9, .1, .1, .1, .05, .1, .2, .04, match_fn, precision = .05)
-    foo.it(1.1)
+    foo = Equilibrium(1, .05, .1, .05, .05, delta = .1, sigma = .2, r = .04, B=.3, precision = .05)
+    foo.it(0.4)
     a = np.copy(foo._values)
     a_market = foo.pick_market()
-    foo.it(2.9)
+    foo.it(0.6)
     b = np.copy(foo._values)
     b_market = foo.pick_market()
+    foo.it(0.8)
+    c = np.copy(foo._values)
+    c_market = foo.pick_market()
     assert(np.max(b) > np.max(a))
-    assert(a_market != b_market)
+    assert(np.max(c) > np.max(b))
+    assert(a_market > b_market)
+    assert(b_market > c_market)
 
 
-def test_unemployment_amounts():
-    foo = Foo(.9, .1, .1, .1, .05, .1, .2, .04, match_fn, precision = .05)
-    print(foo.unemployed_rejects(.5))
-    # assert(False)
+def test_equilibrium_tree_respects_depth():
+    foo = Equilibrium(1, .05, .1, .05, .05, delta = .1, sigma = .2, r = .04, B=.3, precision = .05)
+    tree = foo.equilibrium_tree(3)
+    assert(len(tree) == 3)
+    tree = foo.equilibrium_tree(5)
+    assert(len(tree) == 5)
 
-def test_employed_from():
-    foo = Foo(.9, .1, .1, .1, .05, .1, .2, .04, match_fn, precision = .05)
-    print(foo.employed_from(.5))
-    # assert(False)
+def test_belief_tree_get_amounts():
+    tree = BeliefTree(0, [1,2])
+    l,r = BeliefTree(0, [1,3], [1,1]), BeliefTree(0, [3,5], [1,2])
+    tree.add_children(l,r)
+    assert(np.all(tree.get_amounts('employed') == [2, 3]))
+    assert(np.all(tree.get_amounts('unemployed') == [5, 10]))
 
-def test_fired_employees():
-    foo = Foo(.9, .1, .1, .1, .05, .1, .2, .04, match_fn, precision = .05)
-    print(foo.fired_employees(.5))
-    assert(False)
 
-def test_get_rejects():
-    tree = ['root',
-            ['left1',
-             ['left2', ['left-leaf1', 'right-leaf1'], ['left-leaf2', 'right-leaf2']],
-             ['right2', ['left-leaf3', 'right-leaf3'], ['left-leaf4', 'right-leaf4']]],
-            ['right1',
-             ['left3', ['left-leaf5', 'right-leaf5'], ['left-leaf6', 'right-leaf6']],
-             ['right3', ['left-leaf7', 'right-leaf7'], ['left-leaf8', 'right-leaf8']]]]
-    rejects = get_type(tree)
-    lefties = ['left1', 'left2', 'left-leaf1', 'left-leaf2', 'left-leaf3', 'left-leaf4', 'left3', 'left-leaf5', 'left-leaf6', 'left-leaf7', 'left-leaf8']
-    # assert is the same without caring about order
-    assert(len(rejects) == len(lefties))
-    assert(set(rejects) == set(lefties))
+def test_wages_correctly_gathers_employed():
+    foo = Equilibrium(1, .05, .1, .05, .05, delta = .1, sigma = .2, r = .04, B=.3, precision = .01)
+    foo.equilibrium_tree()
+    wages = sum([i for x in foo._wages.values() for i in x])
+    employed = sum(foo.belief_tree.get_amounts('employed'))
+    assert_approx_equal(wages, employed, 6)
 
-def test_get_winners():
-    tree = ['root',
-            ['left1',
-             ['left2', ['left-leaf1', 'right-leaf1'], ['left-leaf2', 'right-leaf2']],
-             ['right2', ['left-leaf3', 'right-leaf3'], ['left-leaf4', 'right-leaf4']]],
-            ['right1',
-             ['left3', ['left-leaf5', 'right-leaf5'], ['left-leaf6', 'right-leaf6']],
-             ['right3', ['left-leaf7', 'right-leaf7'], ['left-leaf8', 'right-leaf8']]]]
-    rejects = get_type(tree, False)
-    righties = ['right1', 'right2', 'right-leaf1', 'right-leaf2', 'right-leaf3', 'right-leaf4', 'right3', 'right-leaf5', 'right-leaf6', 'right-leaf7', 'right-leaf8']
-    # assert is the same without caring about order
-    assert(len(rejects) == len(righties))
-    assert(set(rejects) == set(righties))
+
+# def test_fired_employees():
+#     foo = Equilibrium(.9, .1, .1, .1, .05, .1, .2, .04, 0.5, precision = .05)
+#     foo.build_distributions(foo.equilibrium_tree(0.5))
+#     print(foo._unemployed)
+#     assert(False)
